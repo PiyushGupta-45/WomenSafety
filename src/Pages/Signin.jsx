b26ad324bf5f5e3f262954c1c382df0788f9097e
PiyushGupta <pgupta3883@gmail.com>
2025-02-28 21:48:29 +0530

    commit

--- src/Pages/Signin.jsx
+++ src/Pages/Signin.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
+
+const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+
+
+    
+  
 
 const Signin = () => {
+    if (!clientId) {
+      console.error("Google Client ID is missing! Check .env file.");
+      return <p style={{ color: "red" }}>Error: Missing Google Client ID</p>;
+    }
+    
+    const handleSuccess = (response) => {
+      console.log("Login Success:", response);
+    };
+    
+    const handleFailure = (error) => {
+      console.error("Login Failed:", error);
+    };
   return (
+    <>
     <div>Signin</div>
+    <GoogleOAuthProvider clientId={clientId}>
+      <div className="flex justify-center items-center min-h-screen">
+        <GoogleLogin onSuccess={handleSuccess} onError={handleFailure} />
+      </div>
+    </GoogleOAuthProvider>
+    </>
   )
 }
 
